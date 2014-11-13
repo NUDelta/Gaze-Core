a829492122e36384659a0af08ae8177c1087116c
TheZallen <zak.allen1009@gmail.com>
2014-11-12 21:20:51 -0600

    added endpoints for TapShare

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -2,6 +2,14 @@ module Api
   module V1
     class AnswersController < ApplicationController
       respond_to :json
+
+      def index
+        if params[:task_id]
+          respond_with Task.find(params[:task_id]).answers
+        else
+          respond_with Task.near([params[:lat], params[:lng]], 1).first.answers
+        end
+      end
       
       def new
         @answer = Answer.new(answer_params)
--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -10,6 +10,20 @@ module Api
         respond_with @tasks.reject { |t| !@user.events.where(:task_id => t.id).empty? }
       end
 
+      def new
+        @task = Task.new(task_params)
+        if @task.save
+          respond_with @task
+        else
+          respond_with nil
+        end
+      end
+
+      private
+
+        def task_params
+          params.require(:task).permit(:lat, :lng, :question)
+        end
     end
   end
 end
