8cee5af9704aa6f207af66ebd128a99aa88b83ce
TheZallen <zak.allen1009@gmail.com>
2014-11-14 09:59:42 -0600

    infowindow

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,10 @@ class HomeController < ApplicationController
   def index
     @tasks = Task.all
     @hash = Gmaps4rails.build_markers(@tasks) do |task, marker|
+      info = task.question + "<br>Number of answers: " + task.answers.count.to_s
       marker.lat task.lat
       marker.lng task.lng
+      marker.infowindow info
     end
   end
 
