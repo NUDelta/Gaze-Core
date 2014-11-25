82babb21a0d16a11257237bff86469854411ffab
TheZallen <zak.allen1009@gmail.com>
2014-11-24 22:40:00 -0600

    better event tracking

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,20 +4,41 @@ module Api
       respond_to :json
       
       def new
-        @task = Task.near([params[:event][:lat], params[:event][:lng]], 0.04).first
+        @task = Task.near([params[:event][:lat], params[:event][:lng]], 0.1).first
         @user = User.where(:username => params[:username]).first
-        @event = Event.new(event_params)
-        @event.update_attributes(:user_id => @user.id)
         if @task && @user.answers.where(:task_id => @task.id).empty?
-          @event.update_attributes(:task_id => @task.id)
-          @event.save
-          respond_with @task
-        elsif @event.save
-          respond_with @event
+          @event = Event.new(event_params)
+          @event.update_attributes(:user_id => @user.id)
+          if @task.distance < 0.04
+            @event.update_attributes(:task_id => @task.id)
+            @event.save
+            respond_with @task
+          elsif @event.save
+            respond_with @event
+          else
+            respond_with nil
+          end
         else
-          respond_with nil
+          error = {:error => "no tasks nearby"}
+          respond_with error
         end
       end
+
+      # def new
+      #   @task = Task.near([params[:event][:lat], params[:event][:lng]], 0.04).first
+      #   @user = User.where(:username => params[:username]).first
+      #   @event = Event.new(event_params)
+      #   @event.update_attributes(:user_id => @user.id)
+      #   if @task && @user.answers.where(:task_id => @task.id).empty?
+      #     @event.update_attributes(:task_id => @task.id)
+      #     @event.save
+      #     respond_with @task
+      #   elsif @event.save
+      #     respond_with @event
+      #   else
+      #     respond_with nil
+      #   end
+      # end
     
       private
 
