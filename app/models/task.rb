b8f55c26559813b9539067629dbad512c1e832a5
TheZallen <zak.allen1009@gmail.com>
2014-11-24 23:50:52 -0600

    dependent destroy

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
   reverse_geocoded_by :lat, :lng
-  has_many :events
-  has_many :answers
+  has_many :events, dependent: :destroy
+  has_many :answers, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :events
-  has_many :answers
+  has_many :events, dependent: :destroy
+  has_many :answers, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
 end
