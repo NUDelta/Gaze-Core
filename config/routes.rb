8b6496baafc61a7887b760ef25595dea765ea70b
Stephen Chan <stephenchan2015@u.northwestern.edu>
2015-01-28 13:48:28 -0600

    modified tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 GazeCore::Application.routes.draw do
-  resources :toys
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
