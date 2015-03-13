43e5bc243b23edebbc0cdfb0e0209d332452d0c9
Stephen Chan <stephenchan2015@u.northwestern.edu>
2015-03-13 12:31:03 -0500

    changed for new food options

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -33,7 +33,7 @@ class Answer < ActiveRecord::Base
         @food_type.task_id = @question_asked.task_id
         @food_type.question_text = "There's free food on floor %s of Ford. What kind is it?" % [@task.floor_number] 
         # TO BE CHANGED AFTER THURSDAY
-        @food_type.question_options = ["THURSDAY_FOOD"]
+        @food_type.question_options = ["FRIDAY_FOOD_AM"]
         @food_type.sequence_num = 4
         @food_type.save
       else
